63372e8e3e45f84a32a499b3016c442b286dedb1
Daniël Knippers <dknippers@gmail.com>
2019-06-25 18:54:52 +0200

    Specified Hamlet compatibility in modinfo

--- src/modinfo.lua
+++ src/modinfo.lua
@@ -9,6 +9,7 @@ api_version_dst = 10
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
+hamlet_compatible = true
 dst_compatible = true
 
 all_clients_require_mod = false
